1f289a59e62879fe90f056f57385ac5d1c64fbee
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-16 20:52:13 +0200

    first contact

---
+++ src/async-functions.js
@@ -0,0 +1,17 @@
+function getTatooineResidents() {
+    console.log('hello')
+}
+
+function promiseMeAString() {
+    console.log('hi')
+}
+
+
+
+
+
+
+module.exports = {
+    getTatooineResidents,
+    promiseMeAString,
+  }
