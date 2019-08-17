599611b2e7a8bce100ee2a7ea6d82ada156a53fe
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-17 19:45:55 +0200

    working version of last test assignment 2

--- src/transform.js
+++ src/transform.js
@@ -2,10 +2,10 @@ function groupAdultsByAgeRange(array) {
     const object = array.reduce((group, obj) => { 
         if (obj.age < 18) {
             return group
-        } else if (obj.age <= 20) {
-            Object.defineProperty(group, '20 and younger', [] )
-            group['20 and younger'].push(obj)
-            return group
+        } else if (obj.age >=18 && obj.age <=20) {
+                group['20 and younger'].push(obj)
+                return group
+            
         } else if (obj.age >=21 && obj.age <=30) {
             group['21-30'].push(obj)
             return group
@@ -21,16 +21,32 @@ function groupAdultsByAgeRange(array) {
         }
         return group    
     }, {
-        
+        '20 and younger': [],
         '21-30': [],
         '31-40': [],
         '41-50': [],
         '51 and older': []
     })
-    console.log(object)
     return object
 }
-console.log(groupAdultsByAgeRange([{name: "Anna", age: 31}, {name: "John", age: 32}, {name: "Hank", age: 60}]))
+console.log(groupAdultsByAgeRange([{ name: "pete", age: 10 },
+{ name: "dove", age: 17 },
+{ name: "dave", age: 18 },
+{ name: "hall", age: 19 },
+{ name: "donn", age: 20 },
+{ name: "trey", age: 21 },
+{ name: "hann", age: 29 },
+{ name: "chew", age: 30 },
+{ name: "cloe", age: 31 },
+{ name: "dart", age: 39 },
+{ name: "this", age: 40 },
+{ name: "dame", age: 41 },
+{ name: "henk", age: 49 },
+{ name: "anna", age: 50 },
+{ name: "olga", age: 51 },
+{ name: "bert", age: 52 },
+{ name: "oldd", age: 120 },
+]))
 // console.log(groupAdultsByAgeRange([{ name: "pete", age: 10 },
 // { name: "dove", age: 17 },]))
 module.exports = {groupAdultsByAgeRange}
