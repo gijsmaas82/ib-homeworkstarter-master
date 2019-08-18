4ac71cdd8f2919f3c092edb398235f688e7e56f5
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-18 14:01:51 +0200

    assignment 3 working

--- src/async-functions.js
+++ src/async-functions.js
@@ -1,15 +1,32 @@
-const fetch = require('node-fetch')
+const superagent = require('superagent')
 const request = require('request')
 
 function getTatooineResidents() {
+  return superagent
+      .get('https://swapi.co/api/planets/1/')
+      .then(residents => {
+        return Promise.resolve(residents.body.residents)
+      })
+      .catch(err => console.log(err))
+  }
 
-}
     
 
-function promiseMeAString() {
-    console.log('hi')
+function promiseMeAString(string) {
+  return new Promise((resolve, reject) => {
+    if (string) {
+      resolve('You kept the Promise!')
+    }
+    reject('You have failed me!')
+  })
 
 }
+
+console.log(promiseMeAString('I promise'))
+
+
+// getTatooineResidents()
+
 
 
 
